5bffaa86ddf85ce3b96070bbf5003d70cbba0011
czucherato <carlos.zucheratto@gmail.com>
2021-11-06 13:22:43 -0300

    Setting up Repository Pattern

---
+++ src/Services/PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using PlatformService.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        private readonly AppDbContext _context;
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            _context.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            return _context.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return _context.SaveChanges() >= 0;
+        }
+    }
+}
